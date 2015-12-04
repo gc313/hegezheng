5ec9cf3e5829ddca141c76529b58fc0956d208e6
gc313 <gc313@foxmail.com>
2015-12-04 21:36:45 +0800

    Ver1.01

--- 合格证_不含部件名.py
+++ 合格证_不含部件名.py
@@ -1,4 +1,7 @@
-#python3 不含部件名的合格证
+# -*- coding: utf-8 -*- 
+# Python3 
+# 不含部件名的合格证
+
 import xlsxwriter
 
 filename = input('文件名：') + '.xlsx'
@@ -56,9 +59,7 @@ format_b2.set_font_name('宋体')
 #设置格式结束----------------------------
 
 
-
 pici = int(input('合格证批次数量：'))  #获取合格证批次数量，便于生成同数量的工作表
-
 
 for i in range(pici):
 	print('----输入第%s批信息，还有%s批----' % (i + 1, pici - i - 1))
@@ -81,30 +82,13 @@ for i in range(pici):
 
 
 	#生成合格证需用到的行数
-	r_0 = 0
-	r_1 = 1
-	r_2 = 2
-	r_3 = 3
-	r_4 = 4
-	r_5 = 5
-	r_6 = 6
-	r_7 = 7
-	r_8 = 8
-	r_9 = 9
-	r_10 = 10
-	#r_11 = 11
-
-
+	r = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 	while col <= (endnum - startnum + 1): #判断合格证生成的数量，数量未达到时无限循环
 		
 		#用到的列数
-		c_0 = 0
-		c_1 = 1
-		c_2 = 2
-		c_3 = 3
-
+		c = [0, 1, 2, 3]
 
 		for i in range(3): #每一行生成三张合格证
 
@@ -112,61 +96,49 @@ for i in range(pici):
 				break
 			sn = str(int(startnum) + col - 1).zfill(4) #合格证上的序列号，默认为4位数字
 
-			worksheet.set_row(r_0, 24)
-			worksheet.set_row(r_1, 24)
-			worksheet.set_row(r_2, 24)
-			worksheet.set_row(r_3, 24)
-			worksheet.set_row(r_4, 24)
-			worksheet.set_row(r_5, 24)
-			worksheet.set_row(r_6, 24)
-			worksheet.set_row(r_7, 24)
-			worksheet.set_row(r_8, 24)
-			worksheet.set_row(r_9, 15)
-			worksheet.set_row(r_10, 5)
+			worksheet.set_row(r[0], 24)
+			worksheet.set_row(r[1], 24)
+			worksheet.set_row(r[2], 24)
+			worksheet.set_row(r[3], 24)
+			worksheet.set_row(r[4], 24)
+			worksheet.set_row(r[5], 24)
+			worksheet.set_row(r[6], 24)
+			worksheet.set_row(r[7], 24)
+			worksheet.set_row(r[8], 24)
+			worksheet.set_row(r[9], 15)
+			worksheet.set_row(r[10], 5)
 			#worksheet.set_row(r_11, 5)
 
-			worksheet.set_column(c_0, c_0, 9)
-			worksheet.set_column(c_1, c_1, 20)
-			worksheet.set_column(c_2, c_2, 0.54)
-			worksheet.set_column(c_3, c_3, 1) #以上设置合格证各行高，各列宽
+			worksheet.set_column(c[0], c[0], 9)
+			worksheet.set_column(c[1], c[1], 20)
+			worksheet.set_column(c[2], c[2], 0.54)
+			worksheet.set_column(c[3], c[3], 1) #以上设置合格证各行高，各列宽
 		
 			
 
 			#填写合格证信息
-			worksheet.merge_range(chr(65 + c_0) + str(r_1) + ':' + chr(67 + c_0) + str(r_1),'公司名称',format_cop) #A1:C1
-			worksheet.merge_range(chr(65 + c_0) + str(r_2) + ':' + chr(67 + c_0) + str(r_2),'产品合格证',format_title)
-			worksheet.merge_range(chr(67 + c_0) + str(r_3) + ':' + chr(67 + c_0) + str(r_9),' ',format_b)
-			worksheet.merge_range(chr(65 + c_0) + str(r_10) + ':' + chr(67 + c_0) + str(r_10),' ',format_b2)
+			worksheet.merge_range(chr(65 + c[0]) + str(r[1]) + ':' + chr(67 + c[0]) + str(r[1]),'公司名称',format_cop) #A1:C1
+			worksheet.merge_range(chr(65 + c[0]) + str(r[2]) + ':' + chr(67 + c[0]) + str(r[2]),'产品合格证',format_title)
+			worksheet.merge_range(chr(67 + c[0]) + str(r[3]) + ':' + chr(67 + c[0]) + str(r[9]),' ',format_b)
+			worksheet.merge_range(chr(65 + c[0]) + str(r[10]) + ':' + chr(67 + c[0]) + str(r[10]),' ',format_b2)
 			
-			worksheet.write_column(chr(65 + c_0) + str(r_3), xiangmu, format_text)
-			worksheet.write(chr(66 + c_0) + str(r_3), pinming, format_text_u)
-			#worksheet.write(chr(66 + c_0) + str(r_4), bujian, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_4), guige, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_5), qianzhui + sn, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_6), pihao, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_7), miejun, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_8), jianyanriqi, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_9), ' ', format_text_u)
+			worksheet.write_column(chr(65 + c[0]) + str(r[3]), xiangmu, format_text)
+			worksheet.write(chr(66 + c[0]) + str(r[3]), pinming, format_text_u)
+			#worksheet.write(chr(66 + c[0]) + str(r[4]), bujian, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[4]), guige, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[5]), qianzhui + sn, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[6]), pihao, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[7]), miejun, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[8]), jianyanriqi, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[9]), ' ', format_text_u)
 
 			col += 1 #每生成一张合格证，计数+1，同时列数+4，准备生成下一张
-			
-			c_0 += 4
-			c_1 += 4
-			c_2 += 4
-			c_3 += 4
-			
-		#一排生成3张合格证后，各行数+12，跳往下一行继续生成合格证
-		r_0 += 11
-		r_1 += 11
-		r_2 += 11
-		r_3 += 11
-		r_4 += 11
-		r_5 += 11
-		r_6 += 11
-		r_7 += 11
-		r_8 += 11
-		r_9 += 11
-		r_10 += 11
-		#r_11 += 12
+
+			for num in range(len(c)):
+				c[num] += 4
+
+		#一排生成3张合格证后，各行数+11，跳往下一行继续生成合格证
+		for num in range(len(r)):
+			r[num] += 11
 
 workbook.close()
--- 合格证_含部件名.py
+++ 合格证_含部件名.py
@@ -1,4 +1,7 @@
-#python3 含部件名的合格证
+# -*- coding: utf-8 -*- 
+# Python3 
+# 含部件名的合格证
+
 import xlsxwriter
 
 filename = input('文件名：') + '.xlsx'
@@ -56,9 +59,8 @@ format_b2.set_font_name('宋体')
 #设置格式结束----------------------------
 
 
-
-pici = int(input('合格证批次数量：'))  #获取合格证批次数量，便于生成同数量的工作表
-
+#获取合格证批次数量，便于生成相同数量的工作表
+pici = int(input('合格证批次数量：'))  
 
 for i in range(pici):
 	print('----输入第%s批信息，还有%s批----' % (i + 1, pici - i - 1))
@@ -73,101 +75,85 @@ for i in range(pici):
 	qianzhui = input('编号前有无前缀？如BAM/C之类，如没有直接回车') #以上获取合格证信息
 
 
-	worksheet = workbook.add_worksheet(pihao + '_' + str(i)) #生成工作表，用批号加序号作为工作表名
+	#生成工作表，用批号加序号作为工作表名
+	worksheet = workbook.add_worksheet(pihao + '_' + str(i)) 
+	
+	#设置页边距
 	worksheet.set_margins(0.6, 0.6, 1.5, 1.5)
 
 
-	col = 1  #生成合格证的计数值
+	#生成合格证的计数值,初始值为1
+	col = 1  
 
 
 	#生成合格证需用到的行数
-	r_0 = 0
-	r_1 = 1
-	r_2 = 2
-	r_3 = 3
-	r_4 = 4
-	r_5 = 5
-	r_6 = 6
-	r_7 = 7
-	r_8 = 8
-	r_9 = 9
-	r_10 = 10
-	r_11 = 11
+	r = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 
-
-
-	while col <= (endnum - startnum + 1): #判断合格证生成的数量，数量未达到时无限循环
+	#判断合格证生成的数量，数量未达到时无限循环
+	while col <= (endnum - startnum + 1): 
 		
 		#用到的列数
-		c_0 = 0
-		c_1 = 1
-		c_2 = 2
-		c_3 = 3
+		c = [0, 1, 2, 3]
+		
 
+		#每一行生成三张合格证
+		for i in range(3): 
 
-		for i in range(3): #每一行生成三张合格证
-
-			if col > (endnum - startnum + 1):  #如果合格证数量已达到，跳出循环
+			#如果合格证数量已达到，跳出循环
+			if col > (endnum - startnum + 1):  
 				break
-			sn = str(int(startnum) + col - 1).zfill(4) #合格证上的序列号，默认为4位数字
+			
+			#合格证上的序列号，默认为4位数字
+			sn = str(int(startnum) + col - 1).zfill(4) 
+			
+			#设置合格证各行高，各列宽
+			worksheet.set_row(r[0], 24)
+			worksheet.set_row(r[1], 24)
+			worksheet.set_row(r[2], 24)
+			worksheet.set_row(r[3], 24)
+			worksheet.set_row(r[4], 24)
+			worksheet.set_row(r[5], 24)
+			worksheet.set_row(r[6], 24)
+			worksheet.set_row(r[7], 24)
+			worksheet.set_row(r[8], 24)
+			worksheet.set_row(r[9], 24)
+			worksheet.set_row(r[10], 15)
+			worksheet.set_row(r[11], 5)
 
-			worksheet.set_row(r_0, 24)
-			worksheet.set_row(r_1, 24)
-			worksheet.set_row(r_2, 24)
-			worksheet.set_row(r_3, 24)
-			worksheet.set_row(r_4, 24)
-			worksheet.set_row(r_5, 24)
-			worksheet.set_row(r_6, 24)
-			worksheet.set_row(r_7, 24)
-			worksheet.set_row(r_8, 24)
-			worksheet.set_row(r_9, 24)
-			worksheet.set_row(r_10, 15)
-			worksheet.set_row(r_11, 5)
-
-			worksheet.set_column(c_0, c_0, 9)
-			worksheet.set_column(c_1, c_1, 20)
-			worksheet.set_column(c_2, c_2, 0.54)
-			worksheet.set_column(c_3, c_3, 1) #以上设置合格证各行高，各列宽
+			worksheet.set_column(c[0], c[0], 9)
+			worksheet.set_column(c[1], c[1], 20)
+			worksheet.set_column(c[2], c[2], 0.54)
+			worksheet.set_column(c[3], c[3], 1) 
 		
 			
-
 			#填写合格证信息
-			worksheet.merge_range(chr(65 + c_0) + str(r_1) + ':' + chr(67 + c_0) + str(r_1),'公司名称',format_cop) #A1:C1
-			worksheet.merge_range(chr(65 + c_0) + str(r_2) + ':' + chr(67 + c_0) + str(r_2),'产品合格证',format_title)
-			worksheet.merge_range(chr(67 + c_0) + str(r_3) + ':' + chr(67 + c_0) + str(r_10),' ',format_b)
-			worksheet.merge_range(chr(65 + c_0) + str(r_11) + ':' + chr(67 + c_0) + str(r_11),' ',format_b2)
+			worksheet.merge_range(chr(65 + c[0]) + str(r[1]) + ':' + chr(67 + c[0]) + str(r[1]),'公司名称',format_cop) #A1:C1
+			worksheet.merge_range(chr(65 + c[0]) + str(r[2]) + ':' + chr(67 + c[0]) + str(r[2]),'产品合格证',format_title)
+			worksheet.merge_range(chr(67 + c[0]) + str(r[3]) + ':' + chr(67 + c[0]) + str(r[10]),' ',format_b)
+			worksheet.merge_range(chr(65 + c[0]) + str(r[11]) + ':' + chr(67 + c[0]) + str(r[11]),' ',format_b2)
 			
-			worksheet.write_column(chr(65 + c_0) + str(r_3), xiangmu, format_text)
-			worksheet.write(chr(66 + c_0) + str(r_3), pinming, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_4), bujian, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_5), guige, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_6), qianzhui + sn, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_7), pihao, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_8), miejun, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_9), jianyanriqi, format_text_u)
-			worksheet.write(chr(66 + c_0) + str(r_10), ' ', format_text_u)
+			worksheet.write_column(chr(65 + c[0]) + str(r[3]), xiangmu, format_text)
+			worksheet.write(chr(66 + c[0]) + str(r[3]), pinming, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[4]), bujian, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[5]), guige, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[6]), qianzhui + sn, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[7]), pihao, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[8]), miejun, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[9]), jianyanriqi, format_text_u)
+			worksheet.write(chr(66 + c[0]) + str(r[10]), ' ', format_text_u)
 
-			col += 1 #每生成一张合格证，计数+1，同时列数+4，准备生成下一张
-			
-			c_0 += 4
-			c_1 += 4
-			c_2 += 4
-			c_3 += 4
+			#每生成一张合格证，计数+1，同时列数+4，准备生成下一张
+			col += 1 
+
+			for num in range(len(c)):
+				c[num] += 4
+
 			
 		#一排生成3张合格证后，各行数+12，跳往下一行继续生成合格证
-		r_0 += 12
-		r_1 += 12
-		r_2 += 12
-		r_3 += 12
-		r_4 += 12
-		r_5 += 12
-		r_6 += 12
-		r_7 += 12
-		r_8 += 12
-		r_9 += 12
-		r_10 += 12
-		r_11 += 12
+		for num in range(len(r)):
+			r[num] += 12
+
 
 workbook.close()
 
